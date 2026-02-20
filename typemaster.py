abc564d1b007e562593c77266d195a260df33b9b
Sunsiscool <luisenriquemelofranjul@gmail.com>
2026-02-19 23:18:56 -0400

    Step 2: Added the game loop and mistake tracking

--- typemaster.py
+++ typemaster.py
@@ -7,3 +7,21 @@ word_bank = [
 ]
 
 game_words = random.sample(word_bank, 15)
+
+mistakes = 0
+correct_count = 0
+
+print("--- The Typing Testo---")
+
+for target_word in game_words:
+    print(f"\nType this exact word: {target_word}")
+    user_typed = input("Here is Your entry: ")
+
+    if user_typed == target_word:
+        print("Correcto!")
+        correct_count += 1
+    else:
+        print("Incorrecto!")
+        mistakes += 1
+    
+    print(f"Current Mistakes: {mistakes}")
