2d56771a5c6717b4ae6c327a77aa67983122e07d
Sunsiscool <luisenriquemelofranjul@gmail.com>
2026-02-19 23:17:06 -0400

    Step 1: Setup word bank and random selection

--- typemaster.py
+++ typemaster.py
@@ -1,8 +1,9 @@
-import randomn
-
+import random
 
 word_bank = [
-    "mateo", "luis", "salma", "paolo", "elver",
-    "marifer", "paleta", "tiktok", "rudo", "mesa",
+    "mateo", "luis", "salma", "paolo", "elver", 
+    "marifer", "paleta", "tiktok", "rudo", "mesa", 
     "silla", "computadora", "celular", "cabello", "ficha"
 ]
+
+game_words = random.sample(word_bank, 15)
