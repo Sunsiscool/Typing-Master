cdf52708a5a8a9b9bf09b6b19e60eda4cccab565
Sunsiscool <luisenriquemelofranjul@gmail.com>
2026-02-19 23:11:33 -0400

    Step 1: Added word bank and random import

---
+++ typemaster.py
@@ -0,0 +1,8 @@
+import randomn
+
+
+word_bank = [
+    "mateo", "luis", "salma", "paolo", "elver",
+    "marifer", "paleta", "tiktok", "rudo", "mesa",
+    "silla", "computadora", "celular", "cabello", "ficha"
+]
